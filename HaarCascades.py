e6ecea2f0fe0e496a32d80012a394c0aa91e6886
luizklitzke1 <luiz.klitzke1@gmail.com>
2024-05-26 18:28:07 -0300

    Ajustado valor de minNeighbors

--- HaarCascades.py
+++ HaarCascades.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 car_cascade = cv2.CascadeClassifier("cascades/cars.xml")
 
 #https://www.geeksforgeeks.org/python-play-a-video-using-opencv/
-card_video_cap = cv2.VideoCapture("samples\cars.avi")
+card_video_cap = cv2.VideoCapture("/content/samples/cars.avi")
 if (card_video_cap.isOpened() == False): 
     print("Erro ao abrir o arquivo de vídeo.") 
 
@@ -15,24 +15,27 @@ frame_idx = 1
 while(card_video_cap.isOpened()): 
     ret, frame = card_video_cap.read() 
 
-    #Erro de ret ou pressionar 'q' para sair
-    if ret == False or cv2.waitKey(25) & 0xFF == ord('q'):
+    if ret == False:
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #Parâmetros: (image , scaleFactor , minNeighbors )
     #https://www.bogotobogo.com/python/OpenCV_Python/python_opencv3_Image_Object_Detection_Face_Detection_Haar_Cascade_Classifiers.php
-    cars = car_cascade.detectMultiScale(gray, 1.03, 1)
+    #minNeighbors como 3 foi o melhor equilibrio de qualidade e falsos positivos que eu encontrei.
+    cars = car_cascade.detectMultiScale(gray, 1.03, 3)
 
     for (x, y ,w ,h) in cars:
-        cv2.rectangle(frame, (x, y),(x + w, y + h), (0, 255 ,0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255 ,0), 2)
 
     plt.title("Frame: " + str(frame_idx) + " | Carros: " + str(len(cars)))
     plt.imshow(frame, 'gray')
     plt.show()
 
     frame_idx += 1
+
+    if cv2.waitKey(1000) == ord('q'):
+        break
 
 card_video_cap.release() 
 cv2.destroyAllWindows() 
